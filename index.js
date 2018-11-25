745e22fd60bb6d8d645c0ee7b1b0315234ec3277
邵瑾瑜 <1009943858@qq.com>
2018-11-25 23:26:47 +0800

    新增封装和解析数据帧

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const net = require('net')
 const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
 
 // websocket握手
 function webSocketHandShake(data, socket) {
@@ -52,6 +54,22 @@ function webSocketHandShake(data, socket) {
 // +---------------------------------------------------------------+
 
 
+// |Opcode  | Meaning                             | Reference |
+// -+--------+-------------------------------------+-----------|
+// | 0      | Continuation Frame                  | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+// | 1      | Text Frame                          | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+// | 2      | Binary Frame                        | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+// | 8      | Connection Close Frame              | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+// | 9      | Ping Frame                          | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+// | 10     | Pong Frame                          | RFC 6455  |
+// -+--------+-------------------------------------+-----------|
+
+
 // 为什么要这么解析？
 // Unit8Array 8位无符号整数值的类型化数组
 // 分析第一位字节 e[0] 二进制
@@ -59,30 +77,89 @@ function webSocketHandShake(data, socket) {
 // 第一位字节十进制为129 二进制为 10000001
 // 右移：按二进制形式把所有的数字向右移动对应位移位数，低位移出(舍弃)，高位的空位补符号位，即正数补零，负数补1。
 // FIN flag 取第一位所以右移7取到第一位 10000001 右移7 00000001
-// &运算符：同时为1才得1，一个为0就为0 eg：1111 & 0011 = 0011
+// &是"与"运算符：同时为1才得1，一个为0就为0 eg：1111 & 0011 = 0011
 // Opcode &15 15的8位二进制为00001111 代表取后面4位 10000001 & 00001111 = 0001
 // PayloadLength 为第二个字节的后7位 &01111111 | &127 | &0x7F
+// websocket划分了三个数据传输界限，PayloadLength 7个bit 最多表示 127byte，有时候这不够用，所以有了拓展PayloadLength
+// 当PayloadLength为126时 用2byte表示扩展长度 最大表示65535byte 约等于64kb
+// 当PayloadLength为127时 用4byte表示扩展长度 最大表示4294967295byte 等于4gb
+// 因此websocket一个数据帧传输数据的最大限制是4gb
+// (e[i++] << 8) +  e[i++] 左移是用来将多个字节的数据拼接在一起
+// eg: 1111111100000000 + 11111110 = 11111111 11111110
+// 掩码
+// ^是异或运算符 如果对应为中任一个操作数是1那结果就是1，如果两个操作数是1那么结果是0
+// eg: 000000001 ^ 00000011 = 00000010
 
+// 解析数据帧
 function decodeDataFrame(e) {
   let i = 0, j, s, frame = {
-    FIN: e[i]>>7,
-    Opcode: e[i++]&15,
-    Mask: e[i]>>7,
-    PayloadLength: e[i++]&0x7F
+    FIN: e[i] >> 7,
+    Opcode: e[i++] & 15,
+    Mask: e[i] >> 7,
+    PayloadLength: e[i++] & 0x7F
   }
   // 处理特殊长度 126和127
   if (frame.PayloadLength === 126) {
-    frame.length = (e[i++] << 8) + e[i++]
+    frame.PayloadLength = (e[i++] << 8) + e[i++]
   }
   if (frame.PayloadLength === 127) {
-    i += 4
-    frame.length = (e[i++] << 24) + (e[i++] << 16) + (e[i++] << 8) +  e[i++]
+    i += 4  //也许是4个字节长度够了 前面4个字节留空
+    frame.PayloadLength = (e[i++] << 24) + (e[i++] << 16) + (e[i++] << 8) +  e[i++]
   }
   // 判断是否使用掩码
   if (frame.Mask) {
     frame.MaskingKey = [e[i++], e[i++], e[i++], e[i++]]
-    console.log(frame.MaskingKey)
+    for (j = 0, s = []; j < frame.PayloadLength; j++) {
+      s.push(e[i + j] ^ frame.MaskingKey[j % 4])
+    }
+  } else {
+    s = e.slice(i, frame.PayloadLength)
   }
+  s = new Buffer(s)
+  if (frame.Opcode == 1) {
+    s = s.toString()
+  }
+  frame.PayloadData = s
+  return frame
+}
+
+// 封装数据帧
+function encodeDataFrame(e) {
+  let i, s = [], o = e.PayloadData, l = o.length
+  s.push((e.FIN << 7) + (e.Opcode & 0xF))
+  if (l < 126) {
+    s.push(l & 0x7F)
+  } else if (l < 0x10000) {
+    s.push(
+      126 & 0x7F,
+      (l & 0xFF00) >> 8,
+      l & 0xFF
+    )
+  } else {
+    s.push(
+      127 & 0x7F,
+      0, 0, 0, 0,
+      (l & 0xFF000000) >> 24,
+      (l & 0xFF0000) >> 16,
+      (l & 0xFF00) >> 8,
+      l & 0xFF
+    )
+  }
+  return Buffer.concat([new Buffer(s), o])
+}
+
+// 生成数据帧
+//  A server must not mask any frames that it sends to the client.
+function generateFrame(PayloadData, Opcode = 1, FIN = 1) {
+  if (!PayloadData) throw new Error('PayloadData is null')
+  return encodeDataFrame({
+    FIN,
+    Opcode,
+    Mask: 0,
+    PayloadData: Object.getPrototypeOf(PayloadData) === Buffer.prototype
+      ? PayloadData
+      : new Buffer(PayloadData),
+  })
 }
 
 const server = net.createServer(socket => {
@@ -94,7 +171,15 @@ const server = net.createServer(socket => {
     if (!socket._webSocketConnect) {
       webSocketHandShake(data, socket)
     } else {
-      decodeDataFrame(data)
+      data = decodeDataFrame(data)
+      switch (data.PayloadData) {
+        case 'pic':
+          socket.write(generateFrame(fs.readFileSync(path.resolve(__dirname, './pic.jpg')), 2))
+          break
+        default:
+          socket.write(generateFrame('Received: ' + data.PayloadData))
+          break
+      }
     }
   })
 }).on('error', (err) => {
